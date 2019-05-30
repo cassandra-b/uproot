a3e5512c059a2aca7771e5e99582fc19407315fd
Nathan Ringo <remexre@gmail.com>
2019-05-29 20:31:59 -0500

    ...

--- src/color.ml
+++ src/color.ml
@@ -7,7 +7,7 @@ let get = Utils.id
 exception NoSuchColor
 
 let convert_color color scheme =
-  if color > Arrays.length scheme then
-    scheme.(color)
-  else
+  if color >= Array.length scheme then
     raise NoSuchColor
+  else
+    scheme.(color)
