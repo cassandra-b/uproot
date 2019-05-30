f1bda1dfe2d511a172c130c6d2ec9f46219e27c2
Cassandra Bleskachek <cassie.bleskachek@gmail.com>
2019-05-30 00:51:05 -0500

    added spanish types and colors

--- src/spanish.ml
+++ src/spanish.ml
@@ -2,15 +2,86 @@ open Language
 
 module Spanish : LANGUAGE = 
 struct
-  type morpheme = Todo
+	let name = "Spanish"
 
-  let name = "Spanish"
+	type tense = 
+    (* indicatives*)
+        | Inf 
+        | Present 
+        | Preterite 
+        | Imperfect 
+        | Future 
+        | Conditional 
+        | Gerund 
+        (* perfect *)
+        | PluPerfect 
+        | PresPerfect 
+        | PretPerfect 
+        | FuturePerfect 
+    (* subjunctives *)
+        | PresSubj 
+        | PretSubj 
+        | ImperfectSubj 
+        | FutureSubj 
+        | CondSubj 
+        (* perfect *)
+        | PluPerfectSubj 
+        | PresPerfSubj 
+        | PretPerfSubj 
+        | FuturePerfSubj
+    (* imperatives *)
+        | Aff
+        | Neg
 
-  let stem_verb (verb:string) : (string * morpheme) list = Utils.todo ()
+	type person = 
+	    | First
+	    | Second
+	    | Third
+	    | FirstPlural
+	    | SecondPlural
+	    | ThirdPlural
 
-  let morpheme_def (morph:morpheme) : text_chunk list = Utils.todo ()
+	type gender = 
+	    | Masc
+	    | Fem
+	    | Neutral
 
-  let morpheme_color (morph:morpheme) : Color.t = Utils.todo ()
+	type arerir =
+	    | Ar
+	    | Er 
+	    | Ir
 
-  let translate (parseList:(string * morpheme) list) : translation_result = Utils.todo ()
+	type regular = bool
+	type reflexive = bool
+
+	type morpheme = 
+		| Root of (regular * arerir * reflexive)
+    	| Conj of (tense * (person list) * arerir)
+    	| Object of (person * gender)
+    	| Reflexive
+
+    type parsing = ((string * morpheme) list) * string
+
+    (* STEM_VERB HELPER FUNCTIONS *)
+
+    	let findReflexive (verb:string) : parsing * bool = 
+			let len = String.length verb in
+			let last2 = String.sub verb (len-2) 2 in
+				match last2 with
+				| "se" -> ((("se", Reflexive)::[], (String.sub verb 0 (len-2))), true)
+				| _ -> (([], verb), false)
+
+	let stem_verb (verb:string) : (string * morpheme) list = Utils.todo ()
+
+	let morpheme_def (morph:morpheme) : text_chunk list = Utils.todo ()
+
+	let morpheme_color (morph:morpheme) : Color.t = 
+		match morph with
+		| Root(_,_,_) -> Color.get 0
+		| Conj(_,_,_) -> Color.get 1
+		| Object(_,_) -> Color.get 2
+		| Reflexive(_) -> Color.get 3
+
+	let translate (parseList:(string * morpheme) list) : translation_result =
+		LanguageNotSupported
 end
